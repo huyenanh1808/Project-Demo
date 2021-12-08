5935f055ddf6541856d17764474352857ad8a880
huyenanh1808 <chient012@gmail.com>
2021-12-08 21:47:25 +0700

    First Commit

---
+++ src/main/java/com/chien/tb/binhchien/BinhchienApplication.java
@@ -0,0 +1,13 @@
+package com.chien.tb.binhchien;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BinhchienApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BinhchienApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chien/tb/binhchien/BinhchienApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chien.tb.binhchien;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BinhchienApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
